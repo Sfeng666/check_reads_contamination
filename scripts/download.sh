fd2eeb2de363d5979c6af2d90c9e2a16cb2206c2
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-09-05 19:28:36 -0500

    download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,3 +1,3 @@
 
 # download the scripts and files
-wget https://entrepot.recherche.data.gouv.fr/api/access/datafile/:persistentId?persistentId=doi:10.57745/Z93CCX
+wget https://entrepot.recherche.data.gouv.fr/api/access/datafile/:persistentId?persistentId=doi:10.57745/Z93CCX gautier_scripts/
