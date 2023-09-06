f15ef1ea4e5ec38609b7787567d55d20dc3c7ca2
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-09-06 01:27:48 -0500

    modify the running script

--- scripts/gautier_scripts/droso_clark/run_droso_clark.sh
+++ scripts/gautier_scripts/droso_clark/run_droso_clark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # this script is to run 'run_fastp_clarkl_clark_and_summarize_results_test.sh'
 cd ../scripts/gautier_scripts/droso_clark
-bash run_fastp_clarkl_clark_and_summarize_results_test.sh CN-Nin
+bash run_fastp_clarkl_clark_and_summarize_results_test.sh CN-Nin >> log.txt 2>&1
