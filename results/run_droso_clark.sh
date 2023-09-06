d8b20a23eb388f7d2c62cc646cebcb8a6555599b
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-09-06 01:19:09 -0500

    scripts to prep input fastq files for fastp

---
+++ results/run_droso_clark.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# this script is to run 'run_fastp_clarkl_clark_and_summarize_results_test.sh'
+cd ../scripts/gautier_scripts/droso_clark
+bash run_fastp_clarkl_clark_and_summarize_results_test.sh CN-Nin
---
+++ scripts/gautier_scripts/prep_conda_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# create the essential conda environment for clark
+conda create --name check_sp_contaminant fastp clark -y
