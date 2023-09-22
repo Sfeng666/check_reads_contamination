8be99c8a0287acc9d54045da95edace66901b18f
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-09-21 22:07:46 -0500

    add two other samples to run contamination check

--- scripts/gautier_scripts/droso_clark/run_droso_clark.sh
+++ scripts/gautier_scripts/droso_clark/run_droso_clark.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # this script is to run 'run_fastp_clarkl_clark_and_summarize_results_test.sh'
-bash run_fastp_clarkl_clark_and_summarize_results_test.sh CN-Nin >> log.txt 2>&1
+# bash run_fastp_clarkl_clark_and_summarize_results_test.sh CN-Nin >> log.txt 2>&1
+bash run_fastp_clarkl_clark_and_summarize_results_test.sh DE-Jen >> log_DE-Jen.txt 2>&1
+bash run_fastp_clarkl_clark_and_summarize_results_test.sh JP-Tok >> log_JP-Tok.txt 2>&1
