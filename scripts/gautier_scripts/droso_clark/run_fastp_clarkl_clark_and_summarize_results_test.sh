c7033f641a9ca7e8a2327d32cbf06f3ec075d863
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-09-30 15:47:35 -0500

    add existing file check to contamination analysis

--- scripts/gautier_scripts/droso_clark/run_fastp_clarkl_clark_and_summarize_results_test.sh
+++ scripts/gautier_scripts/droso_clark/run_fastp_clarkl_clark_and_summarize_results_test.sh
@@ -14,59 +14,64 @@ id=$1 #ID prefix to provide when launching the shel script
 mkdir -p res_clarkl #create a directory named res_clarkl (if it doesn't exist) to store calrkl result
 mkdir -p res_clark  #create a directory named res_clark (if it doesn't exist) to store calrk result
 
-date
+# Check if the analysis has already been done for this sample, and skip the analysis if it has.
+if [ ! -e "$id.html" ]; then
+  date
 
-####################
-#cleanning seqeunce: stdout option to obtain interleaved format further transformed into fasta using awk one liner
-####################
+  ####################
+  #cleanning seqeunce: stdout option to obtain interleaved format further transformed into fasta using awk one liner
+  ####################
 
-$fastp -i $dirfq"/"$id'.fq_1.gz' -I $dirfq"/"$id'.fq_2.gz' --stdout --merge --include_unmerged -h $id'.html' -j $id'.json' | awk '{if(NR%4==2){nn++;{print ">s"nn"\n"$0}}}' - > $dirfq"/"$id'.fasta'
+  $fastp -i $dirfq"/"$id'.fq_1.gz' -I $dirfq"/"$id'.fq_2.gz' --stdout --merge --include_unmerged -h $id'.html' -j $id'.json' | awk '{if(NR%4==2){nn++;{print ">s"nn"\n"$0}}}' - > $dirfq"/"$id'.fasta'
 
-date
+  date
 
-######################
-##CLARK-l analysis
-####################
+  ######################
+  ##CLARK-l analysis
+  ####################
 
-$clarkl -T target_clean.txt -D ./droso_clean_db -O $dirfq"/"$id'.fasta' -R $id -n 1 -m 0 -s 2
+  $clarkl -T target_clean.txt -D ./droso_clean_db -O $dirfq"/"$id'.fasta' -R $id -n 1 -m 0 -s 2
 
-date
+  date
 
-mv ${id}.csv res_clarkl/
+  mv ${id}.csv res_clarkl/
 
 
-######################
-##CLARK analysis
-####################
+  ######################
+  ##CLARK analysis
+  ####################
 
-$clark -T target_clean.txt -D ./droso_clean_db -O $dirfq"/"$id'.fasta' -R $id -n 1 -m 0 -s 2
+  $clark -T target_clean.txt -D ./droso_clean_db -O $dirfq"/"$id'.fasta' -R $id -n 1 -m 0 -s 2
 
-date
+  date
 
-mv ${id}.csv res_clark/
+  mv ${id}.csv res_clark/
 
-rm $dirfq"/"$id'.fasta'
+  rm $dirfq"/"$id'.fasta'
 
-###########
-###summarize statistics (using awk script)
-############
+  ###########
+  ###summarize statistics (using awk script)
+  ############
 
-nkmin='1 5'
-conf='0.9 0.95'
-resdir='res_clark/ res_clarkl/'
+  nkmin='1 5'
+  conf='0.9 0.95'
+  resdir='res_clark/ res_clarkl/'
 
-for dd in $resdir
-do
-cd ./${dd}
- for ii in $nkmin
+  for dd in $resdir
   do
-  for jj in $conf
-   do
-    gawk -f $parseclarkcsv -v nmin_kmer=$ii -v conf_thr=$jj $id.csv > $id"_nkmin"$ii"_conf"$jj".summary"
-   done
- done
-gzip ${id}.csv
-cd ../
-done
+  cd ./${dd}
+  for ii in $nkmin
+    do
+    for jj in $conf
+    do
+      gawk -f $parseclarkcsv -v nmin_kmer=$ii -v conf_thr=$jj $id.csv > $id"_nkmin"$ii"_conf"$jj".summary"
+    done
+  done
+  gzip ${id}.csv
+  cd ../
+  done
 
-date
+  date
+else
+    echo "Skipping because analysis for $sample already exists."
+fi
