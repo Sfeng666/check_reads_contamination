971248084700597e4090a5a866db8f620f0655f9
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-10-01 16:48:01 -0500

    remove -memfree arg from parallel

--- scripts/gautier_scripts/droso_clark/run_parallel_droso_clark.sh
+++ scripts/gautier_scripts/droso_clark/run_parallel_droso_clark.sh
@@ -10,7 +10,7 @@ echo -e "### Contamination check starts at $(date) ###\n" >> $log
 
 { time cat $sample_names \
 | tr '\t' '\n' \
-| parallel --no-notice -j 0 --memfree 200G \
+| parallel --no-notice -j 0 \
 bash $script_runclark {} 1>> $log ;} 2>> $log
 
 echo -e "### Contamination check ends at $(date) ###\n" >> $log
