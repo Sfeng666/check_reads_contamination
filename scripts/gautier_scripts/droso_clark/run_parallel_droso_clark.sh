5bace75d67f51008e2e9e2a5fc1274bbffb1eb08
Sfeng666 <siyuanfeng.bioinfo@gmail.com >
2023-10-02 01:24:11 -0500

    run clark only

--- scripts/gautier_scripts/droso_clark/run_parallel_droso_clark.sh
+++ scripts/gautier_scripts/droso_clark/run_parallel_droso_clark.sh
@@ -2,8 +2,8 @@
 # this script is to run 'run_fastp_clarkl_clark_and_summarize_results_test.sh' to generate contamination stats for every sample
 
 sample_names=../../../data/sample_name.txt   # path to the file containing the sample names (old version)
-script_runclark=run_fastp_clarkl_clark_and_summarize_results_test.sh    # path to the script to run contamination check, given the sample name
-log=run_parallel_droso_clark.log
+script_runclark=run_fastp_clarkl_clark_and_summarize_results_clarkonly.sh    # path to the script to run contamination check, given the sample name
+log=run_parallel_droso_clarkonly.log
 
 # run contamination check for all samples in parallel
 echo -e "### Contamination check starts at $(date) ###\n" >> $log
